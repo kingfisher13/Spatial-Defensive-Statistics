45fde6e70e8fb1148165351b1a5980d31361b4d8
Josh Ryan <mr.josh.j.ryan@gmail.com>
2016-08-05 13:08:56 -0500

    Filter sliders with js refactor

--- js/init.js
+++ js/init.js
@@ -1,87 +1,137 @@
 (function ($, _) {
+  var DATA;
+  var TABLE;
+  var FILTERS = [['Average +/-', 'average'], ['Min +/-', 'min'], ['Max +/-', 'max'], ['Minutes', 'minutes'], ['Games', 'games']];
 
-  /* Build UI (non-table stuff) */
-  var headers = ['ID', 'First', 'Last', 'Team', 'Position', 'Average +/-', 'Min +/-', 'Max +/-', 'Minutes', 'Games'];
-  var filters = ['Average +/-', 'Min +/-', 'Max +/-', 'Minutes', 'Games'];
-
-  headers.forEach(function (h) {
-    $('<th />')
-      .text(h)
-      .appendTo($('.stats-table thead > tr'));
+  $(document).ready(function () {
+    $.getJSON('data/aggregate.json', function (data) {
+      DATA = data;
+      processData();
+      console.log(DATA);
+      buildTable();
+      buildFilters();
+      customEvents();
+    });
   });
 
-  filters.forEach(function (f) {
-    var $fl = $('<div>')
-      .addClass('filter--line')
-      .appendTo($('.filters'));
+  function processData() {
+    DATA.average = {
+      min: Math.round(_.min(DATA.data, function (d) { return d.stats.average; }).stats.average),
+      max: Math.round(_.max(DATA.data, function (d) { return d.stats.average; }).stats.average)
+    };
 
-    $('<span>')
-      .addClass('filter--text')
-      .text(f)
-      .appendTo($fl);
+    DATA.max = {
+      min: Math.round(_.min(DATA.data, function (d) { return d.stats.max; }).stats.max),
+      max: Math.round(_.max(DATA.data, function (d) { return d.stats.max; }).stats.max)
+    };
 
-    var i = f.indexOf(' ') === -1 ? f.length : f.indexOf(' ');
+    DATA.min = {
+      min: Math.round(_.min(DATA.data, function (d) { return d.stats.min; }).stats.min),
+      max: Math.round(_.max(DATA.data, function (d) { return d.stats.min; }).stats.min)
+    };
 
-    $('<input />')
-      .addClass('filter--filter filter--' + f.slice(0, i))
-      .attr('type', 'number')
-      .on('change', function () {
-        console.log('change');
-        table.draw();
-      })
-      .appendTo($fl);
-  });
+    DATA.games = {
+      min: Math.round(_.min(DATA.data, function (d) { return d.games; }).games),
+      max: Math.round(_.max(DATA.data, function (d) { return d.games; }).games)
+    };
+
+    DATA.minutes = {
+      min: Math.round(_.min(DATA.data, function (d) { return d.minutes; }).minutes),
+      max: Math.round(_.max(DATA.data, function (d) { return d.minutes; }).minutes)
+    };
+  }
 
   /* Build Table */
-  var height = $(window).height() - 200; // 100 padding plus 100 headers and footers
+  function buildTable() {
+    /* Headers */
+    var headers = ['ID', 'First', 'Last', 'Team', 'Position', 'Average +/-', 'Min +/-', 'Max +/-', 'Minutes', 'Games'];
+    headers.forEach(function (h) {
+      $('<th />')
+        .text(h)
+        .appendTo($('.stats-table thead > tr'));
+    });
 
-  var table = $('.stats-table').DataTable({
-    processing: true,
-    ajax: 'data/aggregate.json',
-    paging: false,
-    scrollY: height,
-    columns: [
-      { data: 'playerid' },
-      { data: 'firstname' },
-      { data: 'lastname' },
-      { data: 'team' },
-      { data: 'position' },
-      {
-        data: 'stats.average',
-        render: function (data) {
-          return Math.round(data * 100) / 100;
+    /* Table itself */
+    var height = $(window).height() - 200; // 100 padding plus 100 headers and footers
+
+    TABLE = $('.stats-table').DataTable({
+      processing: true,
+      data: DATA.data,
+      paging: false,
+      scrollY: height,
+      columns: [
+        { data: 'playerid' },
+        { data: 'firstname' },
+        { data: 'lastname' },
+        { data: 'team' },
+        { data: 'position' },
+        {
+          data: 'stats.average',
+          render: function (data) {
+            return Math.round(data * 100) / 100;
+          },
         },
-      },
-      { data: 'stats.min' },
-      { data: 'stats.max' },
-      {
-        data: 'minutesPlayed',
-        render: function (data) {
-          return Math.round(data * 100) / 100;
+        { data: 'stats.min' },
+        { data: 'stats.max' },
+        {
+          data: 'minutes',
+          render: function (data) {
+            return Math.round(data * 100) / 100;
+          },
         },
-      },
-      { data: 'gamesPlayed' },
-    ],
-    iDisplayLength: 25,
-    order: [[5, 'desc']],
-  });
+        { data: 'games' },
+      ],
+      iDisplayLength: 25,
+      order: [[5, 'desc']],
+    });
+  }
 
-  /* Filter function */
-  $.fn.dataTable.ext.search.push(
-    function (settings, data, dataIndex) {
-      var min = $('.filter--Games').val();
-      var max = 82;
-      var games = parseFloat(data[9]) || 0; // using the data from the 4th column
+  function buildFilters() {
+    /* Filter UI */
+    FILTERS.forEach(function (f) {
+      var $fl = $('<div>')
+        .addClass('filter--line')
+        .appendTo($('.filters'));
 
-      if ((isNaN(min) && isNaN(max)) ||
-           (isNaN(min) && games <= max) ||
-           (min <= games   && isNaN(max)) ||
-           (min <= games   && games <= max))
-      {
-        return true;
+      $('<span>')
+        .addClass('filter--text')
+        .text(f[0])
+        .appendTo($fl);
+
+      var $slider = $('<div>')
+        .addClass('filter--slider filter--' + f[1])
+        .appendTo($fl);
+
+      var min = DATA[f[1]].min;
+      var max = DATA[f[1]].max;
+
+      noUiSlider.create($slider[0], {
+        start: [min, max],
+        connect: true,
+        range: {
+          min: min,
+          max: max
+        }
+      });
+
+      $slider[0].noUiSlider.on('update', TABLE.draw);
+    });
+  }
+
+  function customEvents() {
+    /* Filter function */
+    $.fn.dataTable.ext.search.push(
+      function (settings, data, dataIndex) {
+        var keepRow = true;
+        FILTERS.forEach(function (f, i) {
+          var sliderVals = $('.filter--' + f[1])[0].noUiSlider.get();
+          var columnData = parseFloat(data[i + 5]) || 0;
+
+          if (parseFloat(sliderVals[0]) > columnData || columnData > parseFloat(sliderVals[1])) keepRow = false;
+        });
+
+        return keepRow;
       }
-
-      return false;
-    }
-  );
+    );
+  }
 })($, _);
